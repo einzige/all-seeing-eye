80a1fd730b7840c4c35c3082ad046f04b54b1d09
Sergei Zinin <zinin@xakep.ru>
2012-12-08 19:17:21 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllSeeingEye::Application.config.secret_token = 'af36b2fdbe9071c27747c3c9df238b7ac9761adf3e7e3524bbd011afbe750426a5eddceb7357bfd3b2e29dbd967b277d7840a46da98fe96e17b3a0c3280caf0d'
