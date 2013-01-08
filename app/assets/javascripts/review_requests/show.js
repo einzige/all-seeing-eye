04bbd4451fd2cceaaf972d836e80006bdc9de602
Sergei Zinin <zinin@xakep.ru>
2013-01-08 18:50:16 +0800

    Selection adjustments

--- app/assets/javascripts/review_requests/show.js
+++ app/assets/javascripts/review_requests/show.js
@@ -13,12 +13,36 @@
 
 
 $(document).ready(function() {
+
+
+    $('.file .tbody').each(function() {
+        var round_column = $(this).find('th.eye:first');
+        round_column.css("border-top-right-radius", "10px");
+        round_column.css("border-top-left-radius", "10px");
+
+        round_column = $(this).find('.past_gutter:eq(1)');
+        round_column.css("border-top-right-radius", "10px");
+
+        round_column = $(this).find('.future_gutter:eq(1)');
+        round_column.css("border-top-left-radius", "10px");
+
+        round_column = $(this).find('th.eye:last');
+        round_column.css("border-bottom-right-radius", "10px");
+        round_column.css("border-bottom-left-radius", "10px");
+
+        round_column = $(this).find('.past_gutter:last');
+        round_column.css("border-bottom-right-radius", "10px");
+
+        round_column = $(this).find('.future_gutter:last');
+        round_column.css("border-bottom-left-radius", "10px");
+    })
+
     var pressed;
     var released;
 
     $('.file th').mousedown(function(e) {
         pressed = $(this).parent('tr');
-
+        pressed.addClass('commented');
         $('.file tr').bind('selectstart', function(event) { event.preventDefault(); });
         $('.file th').bind('mouseover', function() {
             $(this).parent('tr').addClass('commented');
